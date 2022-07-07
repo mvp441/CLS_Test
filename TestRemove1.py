fa3b019c7af6235d5038e659482a77c7e7423113
parmarm <parmarm@wks-l003054.clsi.ca>
2022-07-07 13:33:21 -0600

    Added type testing and descriptors

--- TestRemove1.py
+++ TestRemove1.py
@@ -1,23 +1,27 @@
 import csv
-print('Test 1')
+print('Test 1: initial data1 type')
 data1 = []
 print(type(data1))
-print('Test 2')
+print('Test 2: initial data2 type')
 data2 = {}
 print(type(data2))
 
 with open("SR1_BCaL_8h.csv", 'r') as file:
     csv_file = csv.DictReader(file)
     for row in csv_file:
-        print(dict(row))
+#        print(dict(row))
         data2 = dict(row)
-
         data1.append(dict(row))
 
-print('Test 1')
+print('Test 3: row type')
 print(row)
-print('Test 3')
+
+print('Test 4: final data1 type')
+print(type(data1))
 print(data1[1])
-print('Test 4')
+
+print('Test 5: final data2 type')
 print(type(data2))
+print(data2)
+
 print('Done')
