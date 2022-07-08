864f97625a37ff6ea7891015805be32beca04c15
parmarm <parmarm@wks-l003054.clsi.ca>
2022-07-08 16:55:05 -0600

    Added a line to try and describe without N/A

--- PandasTest.py
+++ PandasTest.py
@@ -1,4 +1,6 @@
 import pandas
 df = pandas.read_csv('SR1_BCaL_8h.csv')
 print(df)
-df.describe()   # quick statistical summary of data
+print('description: ')
+df2 = pandas.Series([df])
+df2.describe(exclude=['N/A'])   # quick statistical summary of data
