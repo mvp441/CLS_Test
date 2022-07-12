a3f47976fbf5e7527080a0d66e4921c2f1726bf5
parmarm <parmarm@wks-l003054.clsi.ca>
2022-07-12 14:54:53 -0600

    Split dataframe into columns and removed NA values

--- PandasTest2.py
+++ PandasTest2.py
@@ -1,3 +1,8 @@
 import pandas as pd
-data = pd.read_csv("SR1_BCaL_8h.csv")
-print(data.columns)
+df = pd.read_csv("SR1_BCaL_8h.csv")
+#print(df.columns)
+fbk = df.iloc[:, [1]]
+fbk = fbk.dropna()
+print(fbk)
+
+
