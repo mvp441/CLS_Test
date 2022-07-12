d914a1a461cd264b111e49ffe135b86c62d21ed3
parmarm <parmarm@wks-l003054.clsi.ca>
2022-07-12 13:50:55 -0600

    Description function working

--- PandasTest.py
+++ PandasTest.py
@@ -1,6 +1,18 @@
 import pandas
 df = pandas.read_csv('SR1_BCaL_8h.csv')
 print(df)
-print('description: ')
+#print('\n Description: ')
+#print(df.describe()) # quick statistical summary of data
+print("\n Series:")
 df2 = pandas.Series([df])
-df2.describe(exclude=['N/A'])   # quick statistical summary of data
+print(df2)
+#df.fillna(0)
+# print(df.sample(n=10))
+#print(df.to_numpy())
+#print("\n")
+#print(df.columns)
+#print("\n")
+
+
+
+
