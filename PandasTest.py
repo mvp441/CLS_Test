7d5661cbd3b10d40b490be7b719b0480a6d01629
parmarm <parmarm@wks-l003054.clsi.ca>
2022-07-12 15:26:30 -0600

    Added mini datafram to check

--- PandasTest.py
+++ PandasTest.py
@@ -1,18 +1,36 @@
 import pandas
+
+#keep_default_na = False
+#na_filter = True
+
 df = pandas.read_csv('SR1_BCaL_8h.csv')
-print(df)
+#print(df)
+
 #print('\n Description: ')
 #print(df.describe()) # quick statistical summary of data
-print("\n Series:")
-df2 = pandas.Series([df])
-print(df2)
-#df.fillna(0)
-# print(df.sample(n=10))
+
+#print("\n Series:")
+#df2 = pandas.Series([df])
+#print(df2)
+
+#print("\n na reaplced? \n")
+
+#print(df.fillna(method="pad")) # filled NaN with data based on surrounding values, only works when printing
+
+
+#print(df)
+
+#print('\n Description: ')
+#print(df.describe()) # quick statistical summary of data
+
+#print(df.sample(n=10))
+
 #print(df.to_numpy())
 #print("\n")
+
 #print(df.columns)
 #print("\n")
 
-
+#df.plot.hist() # histogram for each column
 
 
--- PandasTest2.py
+++ PandasTest2.py
@@ -1,8 +1,28 @@
 import pandas as pd
 df = pd.read_csv("SR1_BCaL_8h.csv")
 #print(df.columns)
+print(df)
+dfmini = df.iloc[1:10,[1]]
+print(dfmini)
+Timestamp = df.iloc[:, [0]]
 fbk = df.iloc[:, [1]]
+mAChange = df.iloc[:, [2]]
+timeConstant = df.iloc[:, [3]]
 fbk = fbk.dropna()
-print(fbk)
+mAChange = mAChange.dropna()
+timeConstant = timeConstant.dropna()
+print(fbk.sample(n=10))
+print(mAChange.sample(n=10))
+print(timeConstant.sample(n=10))
+#print(fbk.loc[1:5])
+#print(mAChange.loc[1:5])
+print(timeConstant.loc[0])
 
 
+# go through column headers
+# save each column title in array
+# convert to string?
+# loop through column titles
+# separate dataframe into individual columns
+# remove na values
+# perform statical analysis
