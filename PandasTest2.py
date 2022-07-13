ef4276bb77c0fa2ef2ca09519e308d76351b56d9
parmarm <parmarm@wks-l003054.clsi.ca>
2022-07-13 15:04:59 -0600

    Checked if N/A values affect the description for the columns and confirmed that the N/A values don't appear to effect the function

--- PandasTest2.py
+++ PandasTest2.py
@@ -3,31 +3,34 @@ df = pd.read_csv("SR1_BCaL_8h.csv")
 #print(df.columns)
 #print(df)
 
-dfmini = df.iloc[1:20,0:4]
-print("dfmini \n")
-print(dfmini)
+dfminiNA = df.iloc[1:20,0:4]
+#print("\n dfmini with N/A \n", dfminiNA.iloc[:,1:4])
 
-Timestamp = dfmini.iloc[:, [0]]
-fbk = dfmini.iloc[:, [1]]
-mAChange = dfmini.iloc[:, [2]]
-timeConstant = dfmini.iloc[:, [3]]
+Timestamp = dfminiNA.iloc[:, [0]]
+fbkNA = dfminiNA.iloc[:, [1]]
+mAChangeNA = dfminiNA.iloc[:, [2]]
+timeConstantNA = dfminiNA.iloc[:, [3]]
 
-print("columns with N/A \n", fbk, "\n", mAChange, "\n", timeConstant)
+print("\n descricption with N/A", "\n", dfminiNA.describe())
+#print("\n descricption with N/A", "\n", dfminiNA.describe(), "\n", fbkNA.describe(), "\n", mAChangeNA.describe(), "\n", timeConstantNA.describe())
+# print("columns with N/A \n", fbkNA, "\n", mAChangeNA, "\n", timeConstantNA)
 
-fbk = fbk.dropna()
-mAChange = mAChange.dropna()
-timeConstant = timeConstant.dropna()
-print("columns without N/A \n", fbk, "\n", mAChange, "\n", timeConstant)
+fbk = fbkNA.dropna()
+mAChange = mAChangeNA.dropna()
+timeConstant = timeConstantNA.dropna()
+
+print("\n descricption without N/A", "\n", fbk.describe(), "\n", mAChange.describe(), "\n", timeConstant.describe())
+# print("columns without N/A \n", fbk, "\n", mAChange, "\n", timeConstant)
 
 # print("\n n=5 sample")
-# print(fbk.sample(n=5))
+# print(fbkNA.sample(n=5))
 # print(mAChange.sample(n=5))
-# print(timeConstant.sample(n=5))
+# print(timeConstantNA.sample(n=5))
 
 # print("\n first 5 position values")
-# print(fbk.loc[0:4])
+# print(fbkNA.loc[0:4])
 # print(mAChange.loc[0:4])
-# print(timeConstant.loc[0:4])
+# print(timeConstantNA.loc[0:4])
 
 
 # go through column headers
