12784bbdadf5784207db229b7e703a69e5721160
parmarm <parmarm@wks-l003054.clsi.ca>
2022-10-06 06:52:25 -0600

    numpy polyline and poly1d and polyfit compile without error now. Doesn't fit all data yet though

--- Plotting.py
+++ Plotting.py
@@ -7,16 +7,29 @@ import matplotlib.pyplot as plt
 def test_func(x, a, b):
     return a * np.sin(b * x)
 
+def Gauss(x, A, B):
+    y = A*np.exp(-1*B*x**2)
+    return y
+
 mpl.use('TkAgg')
 
 
 df = pd.read_csv("SR1_BCaL_8h.csv")
 df.dropna()
 
-dfminiNA = df.iloc[1:20, 0:2]
+
+
+dfminiNA = df.iloc[1:100, 0:2]
 dfminiNA = dfminiNA.dropna()
 TSx = dfminiNA.iloc[:, 0]
 fbky = dfminiNA.iloc[:, 1]
+
+ax = []
+ax = [i for i in range(len(fbky))]
+#print(ax)
+TSx = ax
+# dfminiNA.astype('int32').dtypes
+
 
 # seperate df into mini of each variable to plot
 
@@ -26,24 +39,49 @@ plt.close("all") #close all open plots
 
 #first plot
 #plt.figure(1) #open plot figure
-df.plot() #plot
+
+
+#df.plot() #plot # working still
+
+
 #plt.show() #display plot
 
-print('Done1')
+#print('Done1')
 
 #plt.figure(2)
-dfminiNA.plot() #to show only fbk curve
+#dfminiNA.plot() #to show only fbk curve also working
+#plt.show()
+print('Done 1')
+
 #plt.show()
 #dfminiNA.iloc[1].plot.bar() #causes error
 #dfmNAcs.plot(x="Timestamp", y="fbk") #causes error
 #plt.axline(0,color="k") #causes error
 
-print('Done2')
+print('Done 2')
+
+m1 = np.poly1d(np.polyfit(TSx, fbky, 1))
+m2 = np.poly1d(np.polyfit(TSx, fbky, 2))
+m3 = np.poly1d(np.polyfit(TSx, fbky, 3))
+m4 = np.poly1d(np.polyfit(TSx, fbky, 4))
+m5 = np.poly1d(np.polyfit(TSx, fbky, 5))
+
+polyline = np.linspace(1, 15, 50)
+plt.scatter(TSx, fbky)
+
+plt.plot(polyline, m1(polyline), color='orange')
+plt.plot(polyline, m2(polyline), color='red')
+plt.plot(polyline, m3(polyline), color='purple')
+plt.plot(polyline, m4(polyline), color='blue')
+plt.plot(polyline, m5(polyline), color='green')
+
+
+
 
 plt.show()
 
 #  params, params_covariance = optimize.curve_fit(test_func, TSx, fbky, p0=[2, 2])
-# print(params)
+#  print(params)
 #
 # plt.figure(figsize=(6, 4))
 # plt.scatter(TSx, fbky, label='Data')
@@ -51,5 +89,5 @@ plt.show()
 # plt.legend(loc='best')
 # plt.show()
 #
-# print('Done3')
+print('Done3')
 
