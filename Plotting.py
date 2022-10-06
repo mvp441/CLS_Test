cc71150fc895306b8decb2a24651c77cc0fc9668
parmarm <parmarm@wks-l003054.clsi.ca>
2022-10-06 05:54:47 -0600

    Moved plt.show() so both figure would appear when line reached instead of second figure waiting until first has been closed.

--- Plotting.py
+++ Plotting.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+#from scipy import optimize
+
+def test_func(x, a, b):
+    return a * np.sin(b * x)
 
 mpl.use('TkAgg')
 
@@ -22,17 +27,29 @@ plt.close("all") #close all open plots
 #first plot
 #plt.figure(1) #open plot figure
 df.plot() #plot
-plt.show() #display plot
+#plt.show() #display plot
 
+print('Done1')
 
 #plt.figure(2)
 dfminiNA.plot() #to show only fbk curve
-plt.show()
+#plt.show()
 #dfminiNA.iloc[1].plot.bar() #causes error
 #dfmNAcs.plot(x="Timestamp", y="fbk") #causes error
 #plt.axline(0,color="k") #causes error
 
-print('Done1')
 print('Done2')
-print('Done3')
+
+plt.show()
+
+#  params, params_covariance = optimize.curve_fit(test_func, TSx, fbky, p0=[2, 2])
+# print(params)
+#
+# plt.figure(figsize=(6, 4))
+# plt.scatter(TSx, fbky, label='Data')
+# plt.plot(TSx, test_func(TSx, params[0], params[1]), label='Fitted function')
+# plt.legend(loc='best')
+# plt.show()
+#
+# print('Done3')
 
