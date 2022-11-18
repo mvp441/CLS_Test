30a21dcbb395bd195bb447600b892fd7df7b19b0
Milan <parmarm@corp.lightsource.ca>
2022-11-17 21:35:39 -0500

    Editing Test functions to fix TSx scatter plot testing

--- PandasTest.py
+++ PandasTest.py
@@ -6,20 +6,24 @@ df2 = df
 df2['Timestamp'] = pd.to_datetime(df2['Timestamp'], infer_datetime_format=True)
 df3 = df
 df3['Timestamp'] = pd.to_numeric(df3['Timestamp'])
-dfmini = df3.iloc[1:100, 0:2]
+dfmini = df.iloc[1:100, 0:2]
 dfminiNA = dfmini.dropna()
 #dfminiNA.info()
 #dfminiNA.plot()
 
 TSx = dfminiNA.iloc[:, 0]
 fbky = dfminiNA.iloc[:, 1]
-plt.scatter(TSx, fbky)  # plot data
 
 TSx2 = pd.to_datetime(dfminiNA.Timestamp, infer_datetime_format=True)
 TSx3 = pd.to_timedelta(TSx, unit='ns')
 TSx4 = pd.to_numeric(dfminiNA.Timestamp)
 
+plt.scatter(TSx, fbky)  # plot data
+# plt.scatter(TSx2, fbky)  # plot data
+# plt.scatter(TSx3, fbky)  # plot data
+plt.scatter(TSx4, fbky)  # plot data
 
+print('done')
 
 '''
 ax = [i for i in range(len(fbky))]
