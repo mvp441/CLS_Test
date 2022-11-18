ca0f202aab1564e056e1402a7048b342b5576e83
Milan <parmarm@corp.lightsource.ca>
2022-11-17 21:45:25 -0500

    Changed plot to have xaxis format with date instead of ns

--- PandasTest.py
+++ PandasTest.py
@@ -2,25 +2,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('SR1_BCaL_8h.csv')  # , parse_dates=['Timestamp']) doesn't plot properly when loaded in with Timestamp in datetime format
-df2 = df
-df2['Timestamp'] = pd.to_datetime(df2['Timestamp'], infer_datetime_format=True)
-df3 = df
-df3['Timestamp'] = pd.to_numeric(df3['Timestamp'])
+
 dfmini = df.iloc[1:100, 0:2]
 dfminiNA = dfmini.dropna()
 #dfminiNA.info()
 #dfminiNA.plot()
 
+df2 = df
+df2['Timestamp'] = pd.to_datetime(df2['Timestamp'], infer_datetime_format=True)
+
+df3 = df
+df3['Timestamp'] = pd.to_numeric(df3['Timestamp'])
+
 TSx = dfminiNA.iloc[:, 0]
 fbky = dfminiNA.iloc[:, 1]
 
-TSx2 = pd.to_datetime(dfminiNA.Timestamp, infer_datetime_format=True)
-TSx3 = pd.to_timedelta(TSx, unit='ns')
-TSx4 = pd.to_numeric(dfminiNA.Timestamp)
-
 plt.scatter(TSx, fbky)  # plot data
-# plt.scatter(TSx2, fbky)  # plot data
-# plt.scatter(TSx3, fbky)  # plot data
+
+TSx2 = pd.to_datetime(dfminiNA.Timestamp, infer_datetime_format=True)
+plt.scatter(TSx2, fbky)  # plot data
+
+TSx3 = pd.to_timedelta(TSx, unit='ns')
+plt.scatter(TSx3, fbky)  # plot data
+
+TSx4 = pd.to_numeric(dfminiNA.Timestamp)
 plt.scatter(TSx4, fbky)  # plot data
 
 print('done')
