59a4ac88361abc0049aa5dcff71b2cf8518ab6f9
Milan <parmarm@corp.lightsource.ca>
2022-11-17 21:13:30 -0500

    Added plot using timedelta

--- PandasTest.py
+++ PandasTest.py
@@ -2,31 +2,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('SR1_BCaL_8h.csv')  # , parse_dates=['Timestamp']) doesn't plot properly when loaded in with Timestamp in datetime format
-
-df.plot()
 df2 = df
-df2['Timestamp'] = pd.to_datetime(df2['Timestamp']) #, infer_datetime_format=True)
-# df2.plot()
-plt.show()
-
-dfmini = df.iloc[1:100, 0:2]
+df2['Timestamp'] = pd.to_datetime(df2['Timestamp'], infer_datetime_format=True)
+df3 = df
+df3['Timestamp'] = pd.to_numeric(df3['Timestamp'])
+dfmini = df3.iloc[1:100, 0:2]
 dfminiNA = dfmini.dropna()
+#dfminiNA.info()
+#dfminiNA.plot()
 
 TSx = dfminiNA.iloc[:, 0]
+fbky = dfminiNA.iloc[:, 1]
+plt.scatter(TSx, fbky)  # plot data
+
 TSx2 = pd.to_datetime(dfminiNA.Timestamp, infer_datetime_format=True)
 TSx3 = pd.to_timedelta(TSx, unit='ns')
 TSx4 = pd.to_numeric(dfminiNA.Timestamp)
-
-
-fbky = dfminiNA.iloc[:, 1]
-
-dfminiNA.info()
-dfminiNA.plot()
-dfminiNA['Timestamp'] = pd.to_datetime(dfminiNA['Timestamp'])
-print('after conversion \n')
-dfminiNA.info()
-dfminiNA.plot()
-print('done conversion plot')
 
 
 
