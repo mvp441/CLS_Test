ab61f4b5446c47ae19ff0172a620204e9f909eaf
Milan <parmarm@corp.lightsource.ca>
2022-11-17 20:57:16 -0500

    Added array storing epoch times

--- PandasTest.py
+++ PandasTest.py
@@ -1,15 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('SR1_BCaL_8h.csv', parse_dates=['Timestamp'])
-df.info()
+df = pd.read_csv('SR1_BCaL_8h.csv')  # , parse_dates=['Timestamp']) doesn't plot properly when loaded in with Timestamp in datetime format
+
+df.plot()
+df2 = df
+df2['Timestamp'] = pd.to_datetime(df2['Timestamp']) #, infer_datetime_format=True)
+# df2.plot()
+plt.show()
 
 dfmini = df.iloc[1:100, 0:2]
 dfminiNA = dfmini.dropna()
 
 TSx = dfminiNA.iloc[:, 0]
 TSx2 = pd.to_datetime(dfminiNA.Timestamp, infer_datetime_format=True)
-TSx3 = pd.to_timedelta(TSx2, unit='ns')
+TSx3 = pd.to_timedelta(TSx, unit='ns')
+TSx4 = pd.to_numeric(dfminiNA.Timestamp)
+
 
 fbky = dfminiNA.iloc[:, 1]
 
@@ -31,6 +38,4 @@ x3 = pd.to_datetime(x1)  # single timestamp value
 x4 = x2.iloc[0]  # single timestamp value
 '''
 
-
-print(df2)
 
