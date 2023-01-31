27bf5a593db8c550bc37e56f302cbcf70a0db145
Milan <parmarm@corp.lightsource.ca>
2023-01-30 21:42:49 -0600

    Added functions to CSV class for printing columns and getting descriptions

--- Developing code/lib/CSV.py
+++ Developing code/lib/CSV.py
@@ -7,30 +7,32 @@ class CSVList:
         self.dataframe = pd.DataFrame()
         self.read_csvs()
 
-
     def read_csvs(self):
         for csv in self.csv_files:
             self.__add_csv_to_dataframe(csv)
 
+    # FIX
     def __add_csv_to_dataframe(self, csv):
-        data_frame = pd.read_csv(csv);
+        data_frame = pd.read_csv(csv)
         data_frame["Timestamp"] = data_frame["Timestamp"].apply(pd.to_datetime)
-
         if len(self.dataframe.columns.to_list()) == 0:
             self.dataframe = data_frame
         else:
             self.dataframe = pd.merge(self.dataframe, data_frame, how="outer", on=['Timestamp'])
+
+    # CHECK
     def sort_by_column(self, columns):
         self.dataframe.sort_values(columns)
 
+    # FIX
     def add_csv(self, csv):
         self.csv_files.append(csv)
         self.__add_csv_to_dataframe(csv)
 
-
     def get_column_names(self):
         return self.dataframe.columns.to_list()
 
+    # CHECK
     def interpolate_data(self, method='polynomial', order=None):
         self.dataframe = self.dataframe.interpolate(method='polynomial', order=1)
         self.dataframe = self.dataframe.fillna(0)
@@ -41,6 +43,29 @@ class CSVList:
     def output_dataframe_to_console(self):
         print(tabulate(self.dataframe[1:50], headers='keys', tablefmt='rst'))
 
+    # NEW - CHECK ALL
+    def print_columns(self, column_list):
+        if column_list is None:
+            column_list = self.dataframe.columns
+        for column in column_list:
+            print(self[column])
+            print('\n')
 
+    def get_list_description(self):
+        datafram_description = list()
+        datafram_description.append(self.dataframe.describe())
+        for column in self.dataframe:
+            datafram_description.append(self.dataframe[column].describe())
+        return datafram_description
+
+    def get_dictionary_description(self):
+        datafram_description = {}
+        for column in self.dataframe:
+            datafram_description[column] = self.dataframe[column].describe()
+        return datafram_description
+
+    def drop_na_values(self):
+        for column in self.dataframe:
+            self.dataframe[column] = self.dataframe[column].dropna()
 
 
