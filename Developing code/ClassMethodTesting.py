406e93e6cae94a89afe4751e2098a23177c9cd66
Milan <parmarm@corp.lightsource.ca>
2023-01-30 23:00:42 -0600

    Commit

--- Developing code/ClassMethodTesting.py
+++ Developing code/ClassMethodTesting.py
@@ -3,3 +3,8 @@ from lib import CSV
 csv_list = CSV.CSVList(["../PV Data/Trip 1 data/gLYHVdm+.csv"])
 csv_list.add_csv('../PV Data/Trip 1 data/tdL5QoZo.csv')
 print(csv_list.print_columns())
+
+#Present
+#Pandas3: plotting
+#
+
