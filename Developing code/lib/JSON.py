82a4882924f8f4d5ea3020013ec7cee44500c80b
Milan <parmarm@corp.lightsource.ca>
2023-06-02 23:31:36 -0600

    Removed test json from class function

--- Developing code/lib/JSON.py
+++ Developing code/lib/JSON.py
@@ -75,21 +75,6 @@ class JsonManager:
         self.select_dictionary(filename)
         self.dictionary[key] = value
 
-    def test_load_data_to_dictionary(self):
-        print('starting test of loading json data into dictionary')
-        test_filename_list = load_filenames_from_folder()
-        test_dictionary_with_file_data = json_to_dictionary(test_filename_list[0])
-        test_json_dictionary_list = jsons_to_dictionary_list(test_filename_list)
-        test_select_dictionary = select_dictionary(test_json_dictionary_list, test_filename_list[0])
-        test_add_dictionary_description = add_dictionary_description(test_json_dictionary_list,
-                                                                     test_filename_list[0], "Test description 1")
-        test_add_dictionary_description = add_experiment_number(test_json_dictionary_list, test_filename_list[0], 2)
-        test_add_to_dictionary1 = add_to_dictionary(test_json_dictionary_list, test_filename_list[0], "experiment",
-                                                    3)
-        test_add_to_dictionary2 = add_to_dictionary(test_json_dictionary_list, test_filename_list[0], "description",
-                                                    "Test decription 2")
-        print('finished test of loading json data into dictionary')
-
     def json_to_dataframe(json_file):
         # Need to make second dataframe of just json data (anything in a list)
         # add second dataframe to dictionary entry
